5461fe2322aa9c2ff6d11f09bc4d0d386236f16c
LanaGru <it.kharkov123@gmail.com>
2024-11-24 23:00:07 +0100

    added main

--- main.py
+++ main.py
@@ -1,67 +1,42 @@
+from datetime import datetime
+
+from storage import Storage
 from utils import APIClient
+from analysis import HashTagAnalyzer
 
+# API Facebook business account token
+api_token = 'EAAT9UWWFoQ0BOZB7fzqUuRUJ90QqvbOnBaW6jTTW5TOeVgU0MKfYGs22XWGFv5kCCDvvXoKUNe162jM9tmLxZAOnBImV3CuZCm0wx3UnEZCB7GxINwfNik2KsHJVZA35Jvcjv64NJFhbAyxrioR30x1Aa2ZBqCCrNoPYAgeIu6e7v5JYeLNqL8JJREJLizao0KoBNIZCPHSbOSQsFkZD'
+# Instagram business account
+instagram_business_account = 17841400907433963
+# Hashtags list
+hashcodes = """
+    ballonsmünchen
+    luftballonsmitherz
+    luftballonsmünchen
+    kinderdekoideen
+    kindergeburtstagmünchen
+    luftballonsmithelium
+    ballonstrauß
+    eventdekomünchen
+    """
 
-def main():
-    api_key = "EAAT9UWWFoQ0BO0zAVqoRbMkJw8IHXFO2tZAn0As059TppM6hrBssU37dd3poD9kU2TZAeSmKR63ApJ1FTeH0jCmc7ssYgKj83ANBf47GsPf18sUZAC88BhsH88emMrkCtsgJCZAdNWEVGeodMg3plZBKkcZBNtJ9XAzZBAbDSFiVc4b84ryFold8wjAjeATftr6wZC2kTLWRAHz2jBIZD"
-    user_id = '10235331938303665'
-    account='331485113866425'
-    instagram_business_account = '17841400907433963'
-    hashtags = ["#Python", "#Coding", "#DataScience"]
-    "https://graph.facebook.com/v21.0/me?access_token={{fb_token}}"
-    {
-        "name": "Lana Grunskaya",
-        "id": "10235331938303665"
-    }
+storage = Storage()
+client = APIClient(api_token)
+hashTagAnalyzer = HashTagAnalyzer(client=client, instagram_business_account=instagram_business_account, storage=storage)
 
-    "https://graph.facebook.com/v21.0/me/accounts?access_token={{fb_token}}"
-    {
-        "access_token": "EAAT9UWWFoQ0BO7GSNNhMQEanoWAYj091fKfbtFbOvYbV2TrS074jkihZAaiR9gYjqo0YH0uqXY4ZBDVJ2u0HQMQ1qVVhMtKKPmWYkZCAxClG7HS5VjerwElcvR7TGhb79CGzI4Fu98TMTZAlmnttY2hyHdbvFlThScLVpaKUl0reVC9lcuK7KZAl7adTr5C1186RoCDYdibtLbxOF0nOTyQZDZD",
-        "category": "Одежда (бренд)",
-        "category_list": [
-            {
-                "id": "2209",
-                "name": "Одежда (бренд)"
-            }
-        ],
-        "name": "Вязаный трикотаж",
-        "id": "331485113866425",
-        "tasks": [
-            "ADVERTISE",
-            "ANALYZE",
-            "CREATE_CONTENT",
-            "MESSAGING",
-            "MODERATE",
-            "MANAGE"
-        ]
-    }
+# 1. Load existing database.
+storage.load()
 
-    "https://graph.facebook.com/v21.0/331485113866425?fields=instagram_business_account&access_token={{fb_token}}"
-    {
-        "instagram_business_account": {
-            "id": "17841400907433963"
-        },
-        "id": "331485113866425"
-    }
+# 2. Save time of load data
+load_time = datetime.now()
+print(f'Loading time: {load_time}')
 
-    "https://graph.facebook.com/v21.0/ig_hashtag_search?user_id=17841400907433963&access_token={{fb_token}}&q=pacan"
-    "https://graph.facebook.com/v21.0/17841400907433963/recently_searched_hashtags?access_token={{fb_token}}"
-    "https://graph.facebook.com/v21.0/17843773402049912/top_media?user_id=17841400907433963&fields=id,caption,like_count,comments_count&access_token={{fb_token}}"
-    "https://graph.facebook.com/v21.0/17843773402049912?fields=id,name&access_token={{fb_token}}"
+for hash_tag in hashcodes.split():
+    print(f'Loading statistic for hashtag = [{hash_tag}]')
+    hashTagAnalyzer.compute_statistic(hash_tag, load_time)
+
+# 3. Store data for further analysis
+storage.save()
 
 
 
-    api_client = APIClient(api_key)
-    data_storage = DataStorage("data.json", "data.csv")
-    data_analyzer = DataAnalyzer("data.json")
-
-    for hashtag in hashtags:
-        data = api_client.fetch_hashtag_data(hashtag)
-        if data:
-            data_storage.save_to_json(data)
-            data_storage.save_to_csv(data)
-
-    avg_engagement = data_analyzer.calculate_average_engagement()
-    print(f"Average Engagement Rate: {avg_engagement:.2f}")
-
-if name == "main":
-    main()
